38923fa4e94ec9d0b0879114aacdfbd89c2e0a54
ReVuz <98228399+ReVuz@users.noreply.github.com>
2022-04-12 14:01:49 +0530

    Update 1.py

--- python lab-1/1.py
+++ python lab-1/1.py
@@ -1,16 +1,17 @@
-a=int(input("Enter a four digit number "))
-num=a
-s=num%10
-num=num//10
-u=num%10
-num=num//10
-M=num%10
-num=num//10
-m=num%10
-num=num//10
-print("Sum of ",m,"+",M,"+",u,"+",s," is = ",s+u+M+m)
-print("Reverse of ",a," is: ",s,u,M,m)
-p1=s*M
-p2=u*m
-print("Difference between the product of the digits at the odd and the product of digits at even is ",p2-p1)
-
+num=int(input("Enter a four digit number "))
+def rev(a):
+	num=a
+	s=num%10
+	num=num//10
+	u=num%10
+	num=num//10
+	M=num%10
+	num=num//10
+	m=num%10
+	num=num//10
+	print("Sum of ",m,"+",M,"+",u,"+",s," is = ",s+u+M+m)
+	print("Reverse of ",a," is: ",s,u,M,m)
+	p1=s*M
+	p2=u*m
+	print("Difference between the product of the digits at the odd and the product of digits at even is ",p2-p1)
+abc=rev(num)
