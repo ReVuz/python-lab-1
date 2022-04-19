d54a8031a0b35acefa252bf9bd44c0ad80e25614
ReVuz <98228399+ReVuz@users.noreply.github.com>
2022-04-19 13:15:38 +0530

    Update 5.py

--- python lab-1/5.py
+++ python lab-1/5.py
@@ -1,49 +1,58 @@
-"""Develop a program to read a string and perform the following
-operations:
-• Print all possible substring
-• Print all possible substrings of length K
-• Print all possible substrings of length K with N distinct
-characters
-• Print all palindrome substrings"""
-
 s=str(input("Enter a string "))
 def st(s):
-    l=len(s)
-    print("\nSubstring : ")
-    for i in range(0,l+1):
-        for j in range(i+1,l+1):
-            substring=s[i:j]
-            print(substring)
+	l=len(s)
+	print("\nSubstrings of ",s," are : ")
+	for i in range(0,l+1):
+		for j in range(i+1,l+1):
+			substring=s[i:j]
+			print(substring)
 
 st(s)
-k=int(input("Enter length "))
-def len_k(s,k):
-    l=len(s)
-    for i in range(0,l+1):
-        for j in range(i+1,l+1):
-            substring=s[i:j]
-            if len(substring)==k:
-                print(substring)
+k=int(input("Enter length of substring : "))
+def len_k(s,k):	
+	l=len(s)
+	print("\nSubstrings of length ",k," : ")
+	for i in range(0,l+1):
+		for j in range(i+1,l+1):
+			substring=s[i:j]
+			if len(substring)==k:
+				print(substring)
 len_k(s,k)
-def dist(s,k):
-    l=len(s)
-    print("Distinct substring: ")
-    for i in range(0,l+1):
-        for j in range(i+1,l+1):
-            substring=s[i:j]
-            sett=set(substring)
-            if len(substring)==k:
-                if(len(sett)==k):
-                    print(substring)
-dist(s,k)
-def pal():
-    l=len(s)
-    print("Palindrome substrings are : ")
-    for i in range(0,l+1):
-        for j in range(i+1,l+1):
-            substring=s[i:j]
-            slic_e=substring[::-1]
-            if(slic_e==substring):
-                    print(slic_e,end=",")
-pal()
-
+n=int(input("Enter the number of distinct characters : "))
+def dist(s,k,n):
+	l=len(s)
+	print("Distinct substring: ")
+	for i in range(0,l+1):
+		for j in range(i+1,l+1):
+			substring=s[i:j]
+			sett=set(substring)
+			if (len(substring)==n):
+				if(len(sett)==n):
+					print(substring)
+dist(s,k,n)
+def max_len(s,n):
+	l=len(s)
+	sub_store=[]
+	print("Distinct substring with maximum length : ")
+	for i in range(0,l+1):
+		for j in range(i+1,l+1):
+			substring=s[i:j]
+			sett=set(substring)
+			if(len(sett)==n):
+					sub_store.append(substring)
+	find_max=len(max(sub_store, key=len))
+	for k in range(len(sub_store)):
+		if(len(sub_store[k])==find_max):
+			print(sub_store[k])
+			print("Length of substring : ",find_max)
+max_len(s,n)
+def pal(s):
+	l=len(s)
+	print("Palindrome substrings are : ")
+	for i in range(0,l+1):
+		for j in range(i+1,l+1):
+			substring=s[i:j]
+			slic_e=substring[::-1]
+			if(slic_e==substring):
+				print(slic_e)
+pal(s)
